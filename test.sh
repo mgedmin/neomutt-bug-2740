8ea38e3a9a34317ed9853691bf9aee43e33ba5f8
Marius Gedminas <marius@gedmin.as>
2020-12-01 13:08:14 +0200

    Make the test noninteractive

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ cat inbox2 > /dev/null
 printf "From sender@localhost  Fri Nov 20 11:12:39 2020\nSubject: test message 1\n\nbody\n\n" > inbox3
 printf "From sender@localhost  Fri Nov 20 11:12:39 2020\nSubject: test message 2\n\nbody\n\n" >> inbox3
 neomutt=${1:-neomutt}
-$neomutt -v
-$neomutt -F muttrc -y
+$neomutt -v | head -n 1
+$neomutt -F muttrc -e 'push <quit>' -Z
